cdec7162fdbbef194a614e368d5d207c96db4f5d
camaron6200 <kenakano330@gmail.com>
2025-03-08 22:28:40 +0900

    react routerの追加

---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  //   let navigate = useNavigate();
+  //   const toHobby = () => {
+  //     navigate('/hobby');
+  //   };
+
+  return (
+    <>
+      <header>
+        <h1>申し訳ありません。現在準備中です。</h1>
+      </header>
+      {/* <main></main> */}
+      <footer>&copy; 2025 camaron6200</footer>
+    </>
+  );
+};
+
+export default Home;
